a2a9afa4cc89a889d00717bdb6d9b34933073a5c
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-03-04 19:03:02 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ const books = [
         'author': "Marie-France Léger",
         'description': "Following the death of her abusive ex-boyfriend Malcom, Astra Meredith DuPont moves to a neighboring town in pursuit of a quiet, sedentary lifestyle as an author. While dealing with the traumas of her past, she can't seem to shake off the writer's block that's been eating away at her... that is, until she meets Dr. Noah Praigale, the kind-hearted veterinarian who manages to awaken something beneath the surface.With every chance encounter that pulls the pair together, lines of the…"
     },
-    {
-        'title': "Yellowface",
-        'image': "https://telegra.ph/file/2f996bf6c7f7562dc8dba.jpg",
-        'author': "R.F. Kuang",
-        'description': "With its totally immersive first-person voice, Yellowface grapples with questions of diversity, racism, and cultural appropriation, as well as the terrifying alienation of social media. R.F. Kuang’s novel is timely, razor-sharp, and eminently readable..."
-    }
+    // {
+    //     'title': "Yellowface",
+    //     'image': "https://telegra.ph/file/2f996bf6c7f7562dc8dba.jpg",
+    //     'author': "R.F. Kuang",
+    //     'description': "With its totally immersive first-person voice, Yellowface grapples with questions of diversity, racism, and cultural appropriation, as well as the terrifying alienation of social media. R.F. Kuang’s novel is timely, razor-sharp, and eminently readable..."
+    // }
 
 
 ]
