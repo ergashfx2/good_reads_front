e04e4ccc37ad54396a9c20391df64e23c73681ab
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-03-22 15:42:48 +0500

    commit

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081/api/users"
+  baseURL: "https://readopia-backend.onrender.com/api/users"
 });
 
 const bookCategories = [
